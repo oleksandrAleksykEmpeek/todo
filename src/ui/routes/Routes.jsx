464d7813dd530db36dc8c620895daaeb1e6d193a
oleksandrAleksykEmpeek <96420254+oleksandrAleksykEmpeek@users.noreply.github.com>
2022-06-13 15:53:16 +0300

    feat(home): add home page

--- src/ui/routes/Routes.jsx
+++ src/ui/routes/Routes.jsx
@@ -4,13 +4,14 @@ import PrivateRoute from './components/PrivateRoute';
 import Login from '../pages/Login/Login';
 
 import { paths } from '../../constants/paths';
+import Home from '../pages/Home/Home';
 
 const AppRoutes = () => {
   const { LOGIN } = paths;
   return (
     <Routes>
       <Route path={LOGIN} element={<Login />} />
-      <Route path="/" element={<PrivateRoute component={<div>Hello</div>} />} />
+      <Route path="/" element={<PrivateRoute component={<Home />} />} />
     </Routes>
   );
 };
